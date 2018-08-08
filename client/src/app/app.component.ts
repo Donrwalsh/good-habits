245222451d4c96e345fb5e77ea4580484774785c
Don R Walsh <donrwalsh@gmail.com>
2018-08-08 14:51:29 -0500

    Base class
    Base Class that holds globally available variables. Currently only has a hold of environment name which is fed by the usual environment source.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import { Base } from './base';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent extends Base implements OnInit {
+
+  constructor(private http: HttpClient) {
+    super();
+  }
+
   title = '';
-
-  constructor(private http: HttpClient) {}
-
+  env = this.ENV_NAME;
 
   ngOnInit(): void {
     this.http.get('http://localhost:8080/hello-world').subscribe(data => this.title = data['message']);
---
+++ client/src/app/base.ts
@@ -0,0 +1,7 @@
+import {environment} from '../environments/environment';
+
+export class Base {
+
+  ENV_NAME = environment.envName;
+
+}
